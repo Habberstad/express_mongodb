f0a8d8ab752f4a9034332078e3f3d2bdba7e3a68
Kristoffer Habberstad <kristoffer.habberstad@gmail.com>
2022-04-29 13:04:27 +0200

    List movies test from the database (improved assert)

--- client/__tests__/listMovies.test.js
+++ client/__tests__/listMovies.test.js
@@ -1,12 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { ListMovies } from "../ListMovies";
+import { act } from "react-dom/test-utils";
 
 describe("ListMovies component", function () {
   it("shows loading screen", function () {
     const domElement = document.createElement("div");
     ReactDOM.render(<ListMovies />, domElement);
 
+    expect(domElement.innerHTML).toMatchSnapshot();
+  });
+
+  it("shows movies", async function () {
+    const movies = [{ title: "movie 1" }, { title: "movie 2" }];
+    const domElement = document.createElement("div");
+
+    await act(() => {
+      ReactDOM.render(<ListMovies listMovies={() => movies} />, domElement);
+    });
+
+    expect(
+      Array.from(domElement.querySelectorAll("h3")).map((e) => e.innerHTML)
+    ).toEqual(["movie 1", "movie 2"]);
     expect(domElement.innerHTML).toMatchSnapshot();
   });
 });

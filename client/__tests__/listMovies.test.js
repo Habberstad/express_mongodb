1b141d742630b56e96ba4a20240016c195391377
Kristoffer Habberstad <kristoffer.habberstad@gmail.com>
2022-04-29 12:35:50 +0200

    Complete jest testing for react

--- client/__tests__/listMovies.test.js
+++ client/__tests__/listMovies.test.js
@@ -1,3 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { ListMovies } from "../ListMovies";
+
 describe("ListMovies component", function () {
-  it("shows loading screen", function () {});
+  it("shows loading screen", function () {
+    const domElement = document.createElement("div");
+    ReactDOM.render(<ListMovies />, domElement);
+
+    expect(domElement.innerHTML).toMatchSnapshot();
+  });
 });
